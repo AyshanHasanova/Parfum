70f638264dc5bc2de9a1a5fe562dc09b9f005cc1
unknown <ahsnova557@gmail.com>
2025-12-29 13:45:19 +0400

    deysikilikler edildi

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -6,7 +6,7 @@ import Product from "../models/Product.js";
 
 const seedProducts = async()=>{
 try{
- await mongoose.connect("mongodb://localhost:27017/backend6")
+ await mongoose.connect("mongodb+srv://hsnovaa734_db_user:fl%40me_te%40m1990@cluster0.an9n4ag.mongodb.net/parfumes")
  await Product.deleteMany()
  console.log("zibillik temizlendi")
  await Product.insertMany(products)
